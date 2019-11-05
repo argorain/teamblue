b9779b2921d5749422981dbeb25c4cf4737ac609
Martin Liska <martin.liska@honeywell.com>
2019-11-05 17:06:34 +0100

    List of checklists, on click show checklist items

--- static/js/index.js
+++ static/js/index.js
@@ -15,3 +15,67 @@ ws.onmessage = (e) => {
 ws.onclose = (e) => {
     console.log("WS: CLOSE");
 };
+
+const checklists = [
+    {
+        id: 1,
+        name: "Normal Takeoff",
+        items: [
+            {
+                id: 1,
+                text: "Wing Flaps",
+                value: "20 degrees"
+            },
+            {
+                id: 2,
+                text: "Throttle",
+                value: "100%"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Short Field Takeoff",
+        items: [
+            {
+                id: 3,
+                text: "Wing Flaps",
+                value: "30 degrees"
+            }
+        ]
+    }
+]
+
+$( document ).ready(() => {
+    var $left = $("#left");
+    var $right = $("#right");
+
+    $left.append('<ul class="list-group"></ul>');
+    var $leftMenu = $("#left ul")
+
+    $right.append('<ul class="list-group"></ul>');
+    var $rightList = $("#right ul")
+
+    checklists.forEach((list) => {
+        var listItem = $('<li class="list-group-item" data-id="' + list.id + '">' + list.name + '</li>');
+        $leftMenu.append(listItem);
+        listItem.on('click', (e) => {
+            $rightList.empty();
+            var clickedId = $(e.currentTarget).data().id;
+            console.log(clickedId);
+
+            var items = getItemsOfChecklist(clickedId)
+            items.forEach(item => {
+                $rightList.append('<li class="list-group-item" data-id="' + item.id + '">' + item.text+ '.......' + item.value + '</li>');
+
+            })
+        });
+    })
+
+    var content = $("#content");
+
+})
+
+function getItemsOfChecklist(id) {
+    return checklists.filter((list) => list.id == id)[0].items;
+}
