4a97d8af9dfbb71ca050a995f9dff4236ae8c89b
Vojtech Vladyka <vojtech.vladyka@honeywell.com>
2019-11-05 16:00:12 +0100

    socketio

--- backend.py
+++ backend.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-
+from flask_socketio import SocketIO
 import time
+
 app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello():
@@ -14,6 +17,7 @@ def api():
     return str(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
+    #app.run(debug=True)
 
     
